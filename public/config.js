7a0ddcfdd43c6da986a81eef50c1d5c03f17660a
Cao <112916565+caogens@users.noreply.github.com>
2025-02-08 18:05:42 +0800

    更新 public

---
+++ public/config.js
@@ -0,0 +1,32 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'MJMJJ网站监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2825710-1d08d33871d8cd8d72ef4d60',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 背景图片
+  Image: 'https://ps.ssl.qhimg.com/t025ab47ccd1f7cc6fc.jpg',
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://mjmjj.us.kg'
+    },
+    {
+      text: '监控',
+      url: 'https://stats.uptimerobot.com/KuCPHkw6CJ'
+    },
+  ],
+};
