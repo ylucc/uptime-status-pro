5858a44ef33c171ad3fea92e3c5eb766dd7fb26a
Cao <112916565+caogens@users.noreply.github.com>
2025-02-08 18:00:32 +0800

    Create app.js

---
+++ src/components/app.js
@@ -0,0 +1,45 @@
+import React, { useMemo, useState, createContext } from 'react';
+import Link from './link';
+import Header from './header';
+import UptimeRobot from './uptimerobot';
+import Package from '../../package.json';
+
+export const MonitorContext = createContext();
+
+function App() {
+  const [totalSites, setTotalSites] = useState(0);
+  const [upSites, setUpSites] = useState(0);
+  const [downSites, setDownSites] = useState(0);
+
+  const apikeys = useMemo(() => {
+    const { ApiKeys } = window.Config;
+    if (Array.isArray(ApiKeys)) return ApiKeys;
+    if (typeof ApiKeys === 'string') return [ApiKeys];
+    return [];
+  }, []);
+
+  return (
+    <MonitorContext.Provider value={{ totalSites, setTotalSites, upSites, setUpSites, downSites, setDownSites }}>
+      <Header />
+      <div className='container'>
+        <div id='uptime'>
+          {apikeys.map((key) => (
+            <UptimeRobot key={key} apikey={key} />
+          ))}
+        </div>
+        <div id="footer">
+          <div className='container'>
+            <div className='left'>
+              基于 <Link to='https://uptimerobot.com/' text='UptimeRobot' /> 接口制作，检测频率 5 分钟
+            </div>
+            <div className='right'>
+              &copy; 2020~2025 <Link to='https://mjmjj.us.kg' text='MJMJJ网站监控' />, Version {Package.version}
+            </div>
+          </div>
+        </div>
+      </div>
+    </MonitorContext.Provider>
+  );
+}
+
+export default App;
