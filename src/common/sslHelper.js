8cf06a5c6267cc66a3208b40a44dea2cfc080b64
Cao <112916565+caogens@users.noreply.github.com>
2025-02-09 19:13:50 +0800

    更新 sslHelper.js

--- src/common/sslHelper.js
+++ src/common/sslHelper.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
+// 提取域名的函数
+const extractDomain = (url) => {
+  let domain = url.replace(/^https?:\/\//, ''); // 去除 http:// 或 https:// 前缀
+  domain = domain.split('/')[0]; // 去除路径部分，只保留域名
+  return domain;
+}
+
 // 获取SSL证书信息函数
-export const getSSLInfo = async (domain) => {
+export const getSSLInfo = async (url) => {
   try {
+    const domain = extractDomain(url);
     const response = await axios.get(`http://api.jmjm.tk/api/sslinfo/?url=${encodeURIComponent(domain)}`);
     const data = response.data.data;
 
