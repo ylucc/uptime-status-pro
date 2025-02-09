ea328f5df29bb0204e980a888460d1bcb6c02d35
Cao <112916565+caogens@users.noreply.github.com>
2025-02-09 19:04:11 +0800

    更新 sslHelper.js

--- src/common/sslHelper.js
+++ src/common/sslHelper.js
@@ -6,15 +6,19 @@ export const getSSLInfo = async (domain) => {
     const response = await axios.get(`http://api.jmjm.tk/api/sslinfo/?url=${encodeURIComponent(domain)}`);
     const data = response.data.data;
 
-    return {
-      domain: data.domain,
-      valid_from: data.valid_from,
-      valid_to: data.valid_to,
-      remaining_days: data.remaining_days,
-      is_expired: data.is_expired
-    };
+    if (data) {
+      return {
+        domain: data.domain,
+        valid_from: data.valid_from,
+        valid_to: data.valid_to,
+        remaining_days: data.remaining_days,
+        is_expired: data.is_expired
+      };
+    } else {
+      return { error: '无证书' };
+    }
   } catch (error) {
     console.error('Error fetching SSL certificate information:', error);
-    return { error: 'Unable to retrieve certificate information' };
+    return { error: '无法获取证书信息' };
   }
 };
