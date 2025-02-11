37e57e9c334edc048dfb9c1ae83980291f1571fd
Cao <112916565+caogens@users.noreply.github.com>
2025-02-11 12:37:32 +0800

    更新 config.js

--- public/config.js
+++ public/config.js
@@ -16,7 +16,7 @@ window.Config = {
   ShowLink: true,
 
   // 背景图片
-  Image: 'https://api.jmjm.tk/api/random/?tag=4K',
+  Image: 'http://cdn-hsyq-static.shanhutech.cn/bizhi/staticwp/202010/e4dd0cec9731c326b148c02b2e9af63a--2556948066.jpg',
 
   // 导航栏菜单
   Navi: [
