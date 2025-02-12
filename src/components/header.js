9c267acd3decc500de7f64c8740bf576b87a9039
Cao <112916565+caogens@users.noreply.github.com>
2025-02-12 15:16:21 +0800

    更新 header.js

--- src/components/header.js
+++ src/components/header.js
@@ -34,14 +34,6 @@ useEffect(() => {
           ))}
         </div>
       </div>
-
-      <div className='status-top'>
-        <div className='status-tip' id='status-tip'></div>
-        <p className='status-text' id='status-text'>站点状态加载中</p>
-        <p className='status-text' id='status-down'>部分站点无法运行</p>
-        <p className='status-time' id='status-time-up'>上次更新于&nbsp;<span id='status-last-time'>00&nbsp;:&nbsp;00</span>&emsp;|&emsp;检测频率&nbsp;5&nbsp;分钟</p>
-      </div>
-
       <div className='stats' style={{ textAlign: 'center', marginTop: '20px' }}>
         <div style={{ color: '#4a86e8' }}>网站: {totalSites}</div>
         <div style={{ color: '#3bd672' }}>正常: {upSites}</div>
